b8de82ec1dbd11e28ff037ce6ec450e7120c5716
ibraheem chand <chibraheemchand@gmail.com>
2026-01-02 01:38:47 +0500

    Ignore exe files

---
+++ selectionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void selectSort(int a[], int size){
+
+
+    for (int i = 0; i < size-1; i++)
+    {
+        
+    }
+    
+
+
+  
+}
+
+
+int main (){
+int size = 7 ;
+int a[]= {3,5,4,1,2,7,6};
+
+selectSort(a, size);
+    return 0 ;
+}
