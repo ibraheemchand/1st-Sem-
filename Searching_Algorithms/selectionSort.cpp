f01027787e80627693be55f626c5777f1db409f8
ibraheem chand <chibraheemchand@gmail.com>
2026-01-02 02:12:43 +0500

    Update selection sort and finalize .gitignore

--- Searching_Algorithms/selectionSort.cpp
+++ Searching_Algorithms/selectionSort.cpp
@@ -2,11 +2,29 @@
 using namespace std;
 
 void selectSort(int a[], int size){
-
-
+  int min;
+    int temp;
+   
     for (int i = 0; i < size-1; i++)
-    {
-        
+    {   temp=0;
+       min = INT32_MAX;
+
+       for (int j = i ; j < size; j++)
+       {
+          if (a[j]<min)
+        {
+            min=a[j];
+            temp=j;
+            
+        }
+          
+       }
+       if (temp)
+       {
+        swap(a[temp],a[i]);
+       }
+       
+       
     }
     
 
@@ -14,11 +32,22 @@ void selectSort(int a[], int size){
   
 }
 
+void print(int a[], int size){
+
+for (int i = 0; i < size; i++)
+{
+    cout<<a[i]<<" ";
+}
+
+
+}
 
 int main (){
-int size = 7 ;
-int a[]= {3,5,4,1,2,7,6};
+int size = 15; ;
+int a[15] = {42, 7, 19, 3, 88, 14, 55, 1, 67, 23, 9, 76, 31, 5, 60};
+
 
 selectSort(a, size);
+print(a , size);
     return 0 ;
 }
